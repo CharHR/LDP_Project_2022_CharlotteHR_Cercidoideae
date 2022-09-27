16cf3cdc2bf4ae1b10fe67b70a4ffdb9849b7ddb
CharHR <113125860+CharHR@users.noreply.github.com>
2022-09-27 13:27:27 -0400

    Update LDP_Project_CharlotteHR_download_data.R

--- scripts/LDP_Project_CharlotteHR_download_data.R
+++ scripts/LDP_Project_CharlotteHR_download_data.R
@@ -16,7 +16,7 @@ groundhog.library(raster, date = "2021-08-31")
 groundhog.library(sp, date = "2021-08-31")
 groundhog.library(ggplot2, date = "2021-08-31")
 
-#Data was downloaded from https://www.gbif.org/ on Tuesday July 21st 2020 for the genus Phanera 
+#Data was downloaded from https://www.gbif.org/ on Tuesday July 21st 2020 for the genus Cheniella 
 #DOI: https://doi.org/10.15468/dl.as44t9 
 
-phanera_occurrences<- read.csv('data/GBIF-Phanera-occurrences.csv')
+Cheniella_occurrences<- read.csv('data/CheniellaEdited_Oct_2021.csv')
